c06bb3e3d1d9319bbe405cc200b7847d20ae932e
JasnyGwincior <bermudadragan11@gmail.com>
2025-06-15 23:51:49 +0200

    Add GodotPCKExplorer-master/GodotPCKExplorer-master/Tests/LockedFile.cs 1750024278645

---
+++ GodotPCKExplorer-master/GodotPCKExplorer-master/Tests/LockedFile.cs
@@ -0,0 +1,27 @@
+﻿namespace Tests
+{
+    internal class LockedFile : IDisposable
+    {
+        private FileStream file;
+        bool DeleteAfterDispose = true;
+
+        public LockedFile(string path, bool deleteAfterDispose = true)
+        {
+            file = File.Open(path, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
+            if (!OperatingSystem.IsMacOS())
+                file.Lock(0, 0);
+            DeleteAfterDispose = deleteAfterDispose;
+        }
+
+        public void Dispose()
+        {
+            var path = file.Name;
+            if (!OperatingSystem.IsMacOS())
+                file.Unlock(0, 0);
+            file.Close();
+
+            if (DeleteAfterDispose)
+                File.Delete(path);
+        }
+    }
+}
